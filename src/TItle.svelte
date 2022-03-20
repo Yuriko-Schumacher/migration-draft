a0f007d2dc04ba3090822b890fb3d2d0e8bcf223
Yuriko Schumacher <yuriko.schumacher@gmail.com>
2022-03-19 23:16:30 -0400

    update

--- src/TItle.svelte
+++ src/TItle.svelte
@@ -3,16 +3,15 @@
 </script>
 
 <section>
-  <h1>Migration is Natural</h1>
-  <p>
-    By Joli Holmes, Yuriko Schumacher, and Naveen Kumar Cherukuri
-  </p>
+  <div>
+    <h1>Migration is Natural</h1>
+    <p>
+      By Joli Holmes, Yuriko Schumacher, and Naveen Kumar Cherukuri
+    </p>
+  </div>
   <article>
     <p>
-      The monarch butterfly is often used for a simbol of migration, for its trait of freely crossing the borders humans have created. Migration is natural, and humans, too, have migrated from a country to another, from a region to another.
-    </p>
-    <p>
-      Although countries have their own laws to grant citizenship to migrants, in 2020, more than 85 million people migrated into another country, according to the <a href="https://www.un.org/development/desa/pd/content/international-migrant-stock" target="_blank">United Nations Population Devision</a>.
+      While <a href="https://cadmus.eui.eu//handle/1814/73190" target="_blank">countries have their own laws to grant citizenship to migrants</a>, in 2020, more than 85 million people migrated into another country worldwide, according to the <a href="https://www.un.org/development/desa/pd/content/international-migrant-stock" target="_blank">United Nations Population Devision</a>.
     </p>
     <p>
       On the map below, the butterflies are placed on 21 subregions in the world, and their sizes reflect the number of people who moved there from another region in 2020.
@@ -21,7 +20,6 @@
       Hover over the butterflies to see how many people migrated into the region, and click the butterflies to show the number of migrants by country in the region, as well as to explore various ways to acquire citizeniship in each country.
     </p>
   </article>
-  <span><a href="#map__container">&darr;</a></span>
 </section>
 
 <style>
@@ -30,12 +28,21 @@
   }
   section {
     width: 90%;
-    max-width: 600px;
-    margin: 5rem auto;
+    margin: 0 auto;
+    display: flex;
+    gap: 3rem;
+  }
+  section > * {
+    width: 50%;
+  }
+  div {
+    align-self: flex-end;
+    text-align: right;
   }
   article {
     text-align: left;
     margin-top: 3em;
+    font-size: 0.8rem;
   }
   span {
     font-size: 2em;
