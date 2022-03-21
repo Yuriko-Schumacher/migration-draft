f5434adf233a220ebc922368c3e3b7bc845ffb89
Yuriko Schumacher <yuriko.schumacher@gmail.com>
2022-03-20 22:51:45 -0400

    more minor change

---
+++ src/BigButterflyExplainer.svelte
@@ -0,0 +1,41 @@
+<script>
+  export let isCitizenshipAchieved;
+  
+</script>
+
+<div class="instructions">
+  {#if !isCitizenshipAchieved} 
+    <p class="user-signifier">Click buttons
+      <svg
+        width=18
+        height=18
+        transform="translate(0, 5)"
+      >
+        <circle
+          cx=9
+          cy=9
+          r=5
+          stroke-width=4
+          stroke="black"
+          fill="white"
+        >
+        </circle>
+      </svg>
+      to answer questions. Paths will light up if there is a country-specific law that allows you to acquire citizenship with your condition.
+      <!-- <span class="reset" on:click={handleResetButton}>RESET CONDITIONS</span> -->
+    </p>
+  {:else}
+    <p>
+      Congratulations! You are <span>golden!</span> &#127881;
+    </p>
+  {/if}
+</div>
+
+<style>
+  .instructions {
+    padding: 10px;
+    margin: 10px auto;
+    max-width: 600px;
+    font-size: 18px;
+  }
+</style>
